68a6368a56f1e204e84709644c48ecfe38aba797
Matdu <matheusdudu4@hotmail.com>
2016-04-11 23:23:37 -0300

    Added BaseGameState and TitleScreen class

---
+++ RPGDoo/RPGDoo/RPGDoo/GameScreens/BaseGameState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.GamerServices;
+using XRpgLibrary;
+
+namespace RPGDoo.GameScreens
+{
+        public abstract partial class BaseGameState : GameState
+        {
+            #region Field Region
+            protected Game1 GameRef;
+            #endregion
+
+            #region Properties Region
+            #endregion
+
+            #region Constructor Region
+
+            public BaseGameState(Game1 game, GameStateManager manager)
+                : base(game, manager)
+            {
+                GameRef = (Game1) game;
+            }
+
+            #endregion
+        }
+}
